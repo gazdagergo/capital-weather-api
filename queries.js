03794f09c473aabc7a537d6d29a74253af140f4e
Gergő Gazda <gergo.gazda@agl.group>
2019-06-09 11:48:21 +0200

    handle dbase open close issue - and put db connection config to dotenv

--- queries.js
+++ queries.js
@@ -1,53 +1,44 @@
-/* import { Pool } from 'pg';
-
-const pool = new Pool({
-  dbname: 'dajncrljr7g67q',
-  host: 'ec2-54-75-245-196.eu-west-1.compute.amazonaws.com',
-  port: 5432,
-  user: 'zaexbglyihymig',
-  password: 'b66449413aef3b9c3cdaf9cf9906ee860b7c81b014d30b4bc6fbf72f3b76add3',
-  sslmode: 'require'
-})
-
-export const getCapitals = (req, res)  => {
-  pool.query('SELECT * FROM "capitals"', (error, results) => {
-    if (error) { throw error }
-    res.status(200).json(results.rows);
-  })
-} */
-
-
+import dotenv from 'dotenv';
 import { Client }  from 'pg';
 
-const client = new Client({
-  connectionString: 'postgres://zaexbglyihymig:b66449413aef3b9c3cdaf9cf9906ee860b7c81b014d30b4bc6fbf72f3b76add3@ec2-54-75-245-196.eu-west-1.compute.amazonaws.com:5432/dajncrljr7g67q',
+dotenv.config();
+
+const connectionConfig = {
+  connectionString: process.env.DATABASE_URL,
   ssl: true,
-});
+}
 
 export const getCapitals = (req, res)  => {
+  const client = new Client(connectionConfig);  
   client.connect();
 
   client.query('SELECT * FROM capitals', (err, result) => {
     if (err) throw err;
     res.status(200).json(result.rows);
+    client.end();
   });
 }
 
 
 export const getSavedCities = (req, res)  => {
-  const  query = `SELECT
-    saved_cities.id id,
-    saved_cities.city_id city_id,
-    capitals.capital_name capital_name,
-    capitals.country_code country_code
-  FROM
-      saved_cities
-  INNER JOIN capitals ON saved_cities.city_id = capitals.id;`
-
+  const client = new Client(connectionConfig);  
   client.connect();
+
+  const query = (
+    `SELECT
+      saved_cities.id id,
+      saved_cities.city_id city_id,
+      capitals.capital_name capital_name,
+      capitals.country_code country_code
+    FROM
+        saved_cities
+    INNER JOIN capitals ON saved_cities.city_id = capitals.id;`
+  );
+
   client.query(query, (err, result) => {
     if (err) throw err;
     res.status(200).json(result.rows);
+    client.end();
   });
 
 }
