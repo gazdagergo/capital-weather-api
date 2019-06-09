9479e8f337a8f545d097279d7cbaf63ede782505
Gergő Gazda <gergo.gazda@agl.group>
2019-06-09 19:02:47 +0200

    different city mock data for berlin and bp

--- middlewares.js
+++ middlewares.js
@@ -1,17 +1,32 @@
 export const getCatitalWeather = (req, res) => {
-  let cityId;
-  if (req.params.cityName === 'Berlin') cityId = 196;
-  if (req.params.cityName === 'Budapest') cityId = 61;
+  let json;
 
-  return res.json({
-    cityId,
-    sunriseDate: '09:37',
-    sunsetDate: '15:42',
-    currentTemp: '15',
-    tempMax: '22',
-    tempMin: '12',
-    tempUnit: '˚C',
-    weatherDesc: 'light intensity dizzle',
-    weatherIcon: 'wi_day_thunderstorm',
-  })
+  if (req.params.cityName === 'Berlin') {
+    json = {
+      cityId: 196,
+      sunriseDate: '09:37',
+      sunsetDate: '15:42',
+      currentTemp: '15',
+      tempMax: '22',
+      tempMin: '12',
+      tempUnit: '˚C',
+      weatherDesc: 'thunderstorm',
+      weatherIcon: 'wi_day_thunderstorm',
+    }
+  }
+  if (req.params.cityName === 'Budapest') {
+    json = {
+      cityId: 61,
+      sunriseDate: '09:21',
+      sunsetDate: '16:22',
+      currentTemp: '23',
+      tempMax: '25',
+      tempMin: '18',
+      tempUnit: '˚C',
+      weatherDesc: 'cloud windy',
+      weatherIcon: 'wi-cloudy-windy',
+    }    
+  }
+
+  return res.json(json)
 } 
